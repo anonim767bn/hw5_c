a4040f77f31d9cc2e653cdd62d9328ba65cb229a
anonim767bn <anonim2462@yandex.ru>
2023-05-26 15:33:44 +0300

    add task 4

--- 4.c
+++ 4.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
 void reduce_fraction(int * a, int * b){
-
+    int max_del = 1;
+    int min = *a < *b ? *a : *b;
+    for (int i = min; i > 1; i--){
+        if (*a%i==0 && *b%i==0){
+            max_del = i;
+            break;
+        }
+    }
+    *a = *a/max_del;
+    *b = *b/max_del;
 }
 
 int main() {
-    
+    int a, b;
+    scanf("%d %d", &a, &b);
+    reduce_fraction(&a, &b);
+    printf("%d %d\n", a, b);
 }
