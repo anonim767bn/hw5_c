bc8a5bf39d50a78ce6ba16d81b93be3803909ecf
anonim767bn <anonim2462@yandex.ru>
2023-05-28 23:38:15 +0300

    add coment 1-4

--- 1.c
+++ 1.c
@@ -3,19 +3,24 @@
 int main() {
 	int n;
 	scanf("%d", &n);
+	//считываем длинну массива
 	int arrayFirst[n/2], arraySecond[n/2];
+	//объявляем 2 массива размером в половину n
 
 	for(int i = 0; i < n/2; i++ ){
 		scanf("%d", (arrayFirst+i));
 	}
+	//считываем первую половину ввода с консоли
 	for(int i = 0; i < n/2; i++ ){
         scanf("%d", (arraySecond+i));
     }
+	//считываем 2 половину ввода с консоли
 	for(int i = 0; i < n/2; i++){
 		printf("%d ", arraySecond[i]);
 	}
 	for (int i = 0; i < n/2; i++){
 		printf("%d ", arrayFirst[i]);
 	}
+	//выводим наоборот, сначала вторую затем первую
 	printf("\n");
 }
--- 3.c
+++ 3.c
@@ -4,14 +4,18 @@ struct Move
     int x;
     int y;
 } ;
+//объявляем структуру в которой хранятся 2 переменные х и у
 
 struct Move getFinishPoint(struct Move array[], int n){
     struct Move result = {0, 0};
+    //создаем структуру типа Move со значениями 0 0 - начальная координата чертежника
     for (int i = 0; i < n; i++){
         result.x += array[i].x;
         result.y += array[i].y;
     }
+    //вычисляем координаты суммируя значения из массива
     return result;
+    //возвращаем структуру, которая хранит конечную точку чертежника
 }
 
 int main() {
@@ -23,5 +27,7 @@ int main() {
     int n=2;
     
     finishPoint = getFinishPoint(array, n);
+    //вызвал функцию getFinishPoint с аргументами: указатель на первый элемент массива структур, длини массива. Ее возврат передал переменной структуре finishPoint
     printf("%d %d\n", finishPoint.x, finishPoint.y);
+    //вывел значения в консоль
 }
